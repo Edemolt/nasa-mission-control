19acccf9ec57577702c6bac10229c50069fbd118
Kushagra <kushagra.sahai.s@gmail.com>
2024-10-06 11:17:17 +0530

    new commit on a new branch

--- server/src/app.js
+++ server/src/app.js
@@ -3,29 +3,27 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 
-const  { api }= require('./routes/api');
+const { api } = require('./routes/api');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173'); // Allow requests from this origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
-  
+// Use the cors middleware to allow requests from http://localhost:3000
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true // Allow cookies (if you're using them)
+}));
 
 app.use(morgan('combined'));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname,"..","public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use('/v1', api);
 
-app.get('/*', (req,resp) => {
-    resp.sendFile(path.join(__dirname,"..","public","index.html"));
-})
+// Catch-all route for SPA (Single Page Application)
+app.get('/*', (req, resp) => {
+  resp.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
-
